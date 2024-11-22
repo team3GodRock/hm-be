fa259b1d254eed3c31ad03d6dc3c4b2d2a81e0bf
jumdo12 <jumdo12@gmail.com>
2024-11-22 15:46:24 +0900

    feat: PersonService - 안정성을 위해 Transactional 추가 - Join, findPeople 기능 추가 - 기존 Autowired를 RequiredArgsConstructor로 변경

--- src/main/java/com/example/god/service/PersonService.java
+++ src/main/java/com/example/god/service/PersonService.java
@@ -3,23 +3,31 @@ package com.example.god.service;
 import com.example.god.domain.Person;
 import com.example.god.dto.response.PersonInfoResponseDto;
 import com.example.god.repository.PersonRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class PersonService {
     private final PersonRepository personRepository;
 
-    @Autowired
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    @Transactional
+    public Long join(Person person) {
+        personRepository.save(person); // 새로 저장
+        return person.getId();
+    }
 
+    public List<Person> findPeople(){
+        return personRepository.findAll();
     }
 
     public PersonInfoResponseDto  getPersonInfo(String position){
         Person person = personRepository.findByPosition(position);
         return convertPersonToInfoDto(person);
-
     }
 
     public PersonInfoResponseDto convertPersonToInfoDto(Person person){
@@ -30,8 +38,6 @@ public class PersonService {
                 person.getAffiliation(),
                 person.getSupporting(),
                 person.getAchievement()
-
         );
     }
-
 }
