480e9e0c8246d4c01f96a74701dbf6470d7c662d
jumdo12 <jumdo12@gmail.com>
2024-11-23 13:07:36 +0900

    feat(PersonService): updateAchievement 공략 추가 시 달성률 업데이트 기능추가

--- src/main/java/com/example/god/service/PersonService.java
+++ src/main/java/com/example/god/service/PersonService.java
@@ -4,6 +4,7 @@ import com.example.god.domain.Person;
 import com.example.god.dto.response.PersonInfoResponseDto;
 import com.example.god.dto.response.PersonSimpleDto;
 import com.example.god.repository.PersonRepository;
+import com.example.god.repository.PromiseRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,11 +17,26 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PersonService {
     private final PersonRepository personRepository;
+    private final PromiseRepository promiseRepository;
 
     @Transactional
     public Long join(Person person) {
         personRepository.save(person); // 새로 저장
         return person.getId();
+    }
+
+    @Transactional
+    public void updateAchievement(Person person) {
+        if (person == null) {
+            throw new IllegalArgumentException("Person cannot be null");
+        }
+        Long personId = person.getId();
+        Long achievedCount = promiseRepository.countAchievedPromisesByPersonId(personId);
+        Long totalCount = promiseRepository.countTotalPromisesByPersonId(personId);
+        float achieveRate = (totalCount > 0) ? (float) ((achievedCount * 100) / totalCount) : 0;
+        achieveRate = Math.max(0, Math.min(achieveRate, 100));
+        person.setAchievement(achieveRate);
+        personRepository.save(person);
     }
 
     public List<Person> findPeople(){
@@ -31,7 +47,6 @@ public class PersonService {
         return personRepository.findById(personId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 ID의 Person을 찾을 수 없습니다. ID: " + personId));
     }
-
 
     public PersonInfoResponseDto  getPersonInfo(Long person_id){
         Person person = personRepository.findById(person_id)
